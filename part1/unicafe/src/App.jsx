c4613dc57701d4d54154d5cba920dd6f1cdc5a99
Teite Stephen Sampson <j2sleek@gmail.com>
2025-06-21 22:06:07 +0100

    completed assignment 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = (good - bad) / total || 0 
+  const positive = (good / total) * 100 || 0
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -16,6 +20,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
