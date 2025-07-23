d99e67b1cfd567ee434c88a90aafba3baf76c203
Teite Stephen Sampson <j2sleek@gmail.com>
2025-07-23 20:01:33 +0100

    completed assignment 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -33,6 +33,17 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${reqDate}</p>`);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        response.send(person);
+    } else {
+        response.status(404).end();
+    }
+})
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
