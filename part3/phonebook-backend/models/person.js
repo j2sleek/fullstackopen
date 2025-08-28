9383624a6e8d1818b8ae525d0596ff2d4f22bc62
Teite Stephen Sampson <j2sleek@gmail.com>
2025-08-28 13:32:28 +0100

    completed assignment 3.20

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -15,9 +15,20 @@ mongoose.connect(url)
   const personSchema = new mongoose.Schema({
     name: {
       type: String,
-      minLength: 3
+      minLength: 3,
+      required: [true, 'Name required']
     },
-    number: String,
+    number: {
+      type: String,
+      minLength: 8,
+      validate: {
+        validator: function(v) {
+          return /^\d{2,3}-\d+$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: [true, 'User phone number required (###-########)']
+    },
   });
 
   personSchema.set('toJSON', {
