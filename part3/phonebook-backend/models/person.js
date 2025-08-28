6912ab2095ea74642d13fbf9e9a96b208cc50017
Teite Stephen Sampson <j2sleek@gmail.com>
2025-08-28 13:36:53 +0100

    completed assignment 3.21

--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -27,7 +27,7 @@ mongoose.connect(url)
         },
         message: props => `${props.value} is not a valid phone number!`
       },
-      required: [true, 'User phone number required (###-########)']
+      required: [true, 'User phone number required (in format ###-########)']
     },
   });
 
