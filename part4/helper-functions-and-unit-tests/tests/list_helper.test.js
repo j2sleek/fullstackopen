6f4a34344913b4dae05e43e6e46ad389bf570f11
Teite Stephen Sampson <j2sleek@gmail.com>
2025-09-02 18:44:04 +0100

    update: completed assignment 4.5

--- part4/helper-functions-and-unit-tests/tests/list_helper.test.js
+++ part4/helper-functions-and-unit-tests/tests/list_helper.test.js
@@ -120,6 +120,13 @@ describe('favorite blog', () => {
     ]
 
     test('returns the blog with the most likes', () => {
-        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), 12)
+        assert.deepStrictEqual(listHelper.favoriteBlog(blogs), {
+            _id: "5a422b3a1b54a676234d17f9",
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+            likes: 12,
+            __v: 0
+        })
     })
 })
