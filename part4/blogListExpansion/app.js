592253d2145f8de84509c43af16b25da34a52e1f
Teite Stephen Sampson <j2sleek@gmail.com>
2025-10-24 13:15:30 +0100

    completed assignment 2.18

--- part4/blogListExpansion/app.js
+++ part4/blogListExpansion/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -15,6 +16,7 @@ mongoose.connect(config.MONGODB_URI)
   .catch(error => logger.error('Error connecting to db:', error.message))
 
 app.use(express.json())
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
