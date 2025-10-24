376e8f842527e97dcaa2993eded8cd766d7c7963
Teite Stephen Sampson <j2sleek@gmail.com>
2025-10-24 12:44:33 +0100

    completed assignment 4.17

--- part4/blogListExpansion/controllers/blogs.js
+++ part4/blogListExpansion/controllers/blogs.js
@@ -1,22 +1,35 @@
 const blogsRouter = require('express').Router()
 const { response } = require('../app')
-const blog = require('../models/blog')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   try {
+    const allUsers = await User.find({})
+    const user = allUsers[0]
+    request.body.user = user.id
+
     const blog = new Blog(request.body)
     const res = await blog.save()
+
+    user.blogs = user.blogs.concat(res._id)
+    await user.save()
+
     response.status(201).json(res)
+
   } catch (error) {
     if (error.name === 'ValidationError') {
       response.status(400).json({
         message: 'title or url validation failed'
+      }) 
+    }else {
+      response.status(400).json({
+        message: error.message
       })
     }
   } 
